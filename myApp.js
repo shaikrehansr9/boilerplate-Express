ae4937d4cd62f602d80a0ed7124da8d4e1d3f8eb
shaik rehan <shaikrehan91000@gmail.com>
2025-06-17 22:52:59 +0530

    third commit

--- myApp.js
+++ myApp.js
@@ -2,9 +2,12 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
-app.get('/',(req,res)=> {
-  res.sendFile(path.join(__dirname +"views","index.html"));
+app.get("/", (req, res) => {
+  const absolutePath = path.join(__dirname, "views", "index.html");
+  console.log("Serving file from:", absolutePath); // Debugging
+  res.sendFile(absolutePath);
 });
+
 
 
 
