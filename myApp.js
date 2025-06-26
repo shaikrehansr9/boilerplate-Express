bbcb5b042f8fcdf08f2f1f97f2fa485958c18224
shaik rehan <shaikrehan91000@gmail.com>
2025-06-26 23:01:45 +0530

    fourth commit

--- myApp.js
+++ myApp.js
@@ -1,23 +1,7 @@
-let express = require('express');
+const express = require('express');
+const app = express();
+const path = require('path'); // Required to work with file paths
 
-let app = express.Router();
-// console.log("Hello World");
-
-// app.get("/", (req, res) => {
-//   const absolutePath = path.join(__dirname, "views/index.html");
-//   console.log("Received GET request for /");
-//   console.log("Serving file from:", absolutePath);
-//   res.sendFile(absolutePath, (err) => {
-//     if (err) {
-//       console.error("Error sending file:", err);
-//     } else {
-//       console.log("File sent successfully!");
-//     }
-//   });
-// 
-//  module.exports = app;
-
-const path = require('path'); // Import the path module
 
 app.get('/', (req, res) => {
   const absolutePath = path.join(__dirname, 'views', 'index.html'); // Calculate the absolute path
