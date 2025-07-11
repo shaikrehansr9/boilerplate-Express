634d52b3ff8f5cda78e0ddb11201d228d2f26519
shaik rehan <shaikrehan91000@gmail.com>
2025-07-11 12:01:15 +0530

    11th commit

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express.Router();
 const path = require('path'); 
+require('dotenv').config();
+
 
 app.use('/public',express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
@@ -8,6 +10,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'public','index.html')); // Send the file as a response
 });
 app.get('/json',(req,res)=>{
-  res.json({"message": "Hello json"})
+  let msg = "Hello json"
+  if(process.env.MESSAGE_STYLE==='uppercase'){
+    msg=msg.toUpperCase();
+  }
+  res.json({"message": msg})
 })
 module.exports = app;
