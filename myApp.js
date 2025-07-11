b07d7f0c547f7a46457bc0db0a5a5bb5804099d5
shaik rehan <shaikrehan91000@gmail.com>
2025-07-11 12:07:12 +0530

    11th commit

--- myApp.js
+++ myApp.js
@@ -14,6 +14,6 @@ app.get('/json',(req,res)=>{
   if(process.env.MESSAGE_STYLE==='uppercase'){
     msg=msg.toUpperCase();
   }
-  res.json({"message": msg})
+  res.json({msg})
 })
 module.exports = app;
